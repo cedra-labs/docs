ff880d3440e45e56ae3bcff40d440f61a323e56f
Yaroslav Dvorovenko <yaroslav.dvorovenko@chromaway.com>
2025-06-23 14:55:14 +0200

    Update links in footer

--- src/components/ArticleFooter/index.tsx
+++ src/components/ArticleFooter/index.tsx
@@ -16,7 +16,7 @@ export default function ArticleFooter(): React.ReactNode {
           <div className={styles.actions}>
             <Link
               className="button button--primary button--md"
-              to="https://docs.cedra.network/"
+              to="https://github.com/cedra-labs/docs/pulls"
               target="_blank"
             >
               Improve Docs & Report Issues
